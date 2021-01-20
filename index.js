f46ab21fd8f6a9b76d8ee9a877558e9f7bfbb555
NathanLeadillHome <seclyps@hotmail.com>
2021-01-20 15:58:56 +0000

    Creating s3 object

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 let accessKey = '';
 let secretKey = '';
+let s3;
 
 exports.setAWSData = (aKey, sKey) => {
     accessKey = aKey;
     secretKey = sKey;
+    s3 = new AWS.S3({
+        accessKeyId: aKey,
+        secretAccessKey: sKey,
+    })
 }
