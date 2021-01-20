661fdc662febe5c96141291a2a57c9d63af1d23a
NathanLeadillHome <seclyps@hotmail.com>
2021-01-20 21:12:42 +0000

    Update to version

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ let accessKey = '';
 let secretKey = '';
 let s3;
 
-exports.setAWSData = (aKey, sKey) => {
+const setAWSData = (aKey, sKey) => {
     accessKey = aKey;
     secretKey = sKey;
     s3 = new AWS.S3({
         accessKeyId: aKey,
         secretAccessKey: sKey,
     })
+    console.log(s3);
 }
+
+module.exports = { setAwsData }
